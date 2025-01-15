6a2a2c2b5a5365f6f49f2253e0461117e7cbf69d
Coco29dev <10059@holbertonstudents.com>
2025-01-15 10:16:45 +0100

    Code fonctionnel task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,5 +8,5 @@ if last_number > 5:
 elif last_number == 0:
     message += " and is 0"
 elif last_number < 6 and last_number != 0:
-    message += " and is less than 6 and 0"
+    message += " and is less than 6 and not 0"
 print(message)
