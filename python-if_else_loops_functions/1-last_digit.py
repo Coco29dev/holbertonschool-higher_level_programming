dd00310bc2cd4de6d5c685a76b097dd3f7b092a7
Coco29dev <10059@holbertonstudents.com>
2025-01-15 10:22:39 +0100

    modification else pour checker task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,6 +7,6 @@ if last_number > 5:
     message += " and is greater than 5"
 elif last_number == 0:
     message += " and is 0"
-elif last_number < 6 and last_number != 0:
+else:
     message += " and is less than 6 and not 0"
 print(message)
