3ded6503423cc7aeef6e3fbd7d8a76ed0a5256d4
Coco29dev <10059@holbertonstudents.com>
2025-01-15 20:28:37 +0100

    modification variable

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,12 +2,12 @@
 def uppercase(str):
     # Parcours chaque caractère de la chaîne
     # Si le caractère est une lettre minuscule, convertir en majuscule
-    result = ""
+    resultat = ""
     for c in str:
         if 97 <= ord(c) <= 122:
             # Convertir la minuscule en majuscule avec - 32 à son code ASCII
-            result += chr(ord(c) - 32)
+            resultat += chr(ord(c) - 32)
         else:
             # Si déjà une majuscule ou un autre caractère afficher tel quel
-            result += c
-    print("{}".format(result))
+            resultat += c
+    print("{}".format(resultat))
