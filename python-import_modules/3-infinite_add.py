708af5bb431c4b1bee2ced64eee4ff1370b1faef
Coco29dev <10059@holbertonstudents.com>
2025-01-17 14:58:09 +0100

    pycodestyle

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     resultat = 0
 
     for i in range(1, len(sys.argv)):
-         resultat += int(sys.argv[i])
+        resultat += int(sys.argv[i])
     print("{}".format(resultat))
