2080e3da3395021c8885802f9a1b5403b4850f84
Coco29dev <10059@holbertonstudents.com>
2025-01-17 14:56:59 +0100

    code fonctionnel task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    resultat = 0
+
+    for i in range(1, len(sys.argv)):
+         resultat += int(sys.argv[i])
+    print("{}".format(resultat))
