8b65bca81cb53041f53c3dd7b563eff97762b3b2
Coco29dev <10059@holbertonstudents.com>
2025-01-17 13:01:40 +0100

    code fonctionnel task 1

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
+# Ici, on importe la fonction 'add' depuis le fichier 'add_0.py'.
 if __name__ == "__main__":
+	# Cette condition vérifie si ce script est exécuté directement, et non importé comme module.
     a = 1
     b = 2
     resultat = add(a, b)
-    print("{} + {} = {}".format(a, b, resultat))
+    # On appelle la fonction 'add' en passant 'a' et 'b' en arguments 
+    # et on stocke le résultat dans 'resultat'.
+    print("{} + {} = {}".format())
+    # On affiche le résultat de l'addition dans le format "1 + 2 = 3".
