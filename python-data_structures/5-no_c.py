8213929d438da1e0d2e3fbb5518a210e1bf27aca
Coco29dev <10059@holbertonstudents.com>
2025-01-20 15:12:09 +0100

    rajout commentaire

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    # Utilise la méthode translate() pour supprimer 'c' et 'C' de la chaîne
+    # ord('c') et ord('C') renvoient les codes ASCII des caractères à supprimer
+    # On remplace ces caractères par None, ce qui les supprime de la chaîne
     return my_string.translate({ord('c'): None, ord('C'): None})
