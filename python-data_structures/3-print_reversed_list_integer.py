56f464c314dedd86265c57aaacd1873c7bc3d027
Coco29dev <10059@holbertonstudents.com>
2025-01-20 15:16:52 +0100

    rajout commentaire

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    # Utilise la fonction reversed() pour parcourir la liste à l'envers.
+    # Pour chaque nombre dans la liste inversée
+    # affiche-le sous forme d'entier formaté.
+    # "{:d}".format(num) est utilisé pour
+    # afficher l'élément sous forme d'entier.
     for num in reversed(my_list):
         print("{:d}".format(num))
