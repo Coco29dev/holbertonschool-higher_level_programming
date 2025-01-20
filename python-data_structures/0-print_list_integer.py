48eeee3f382d920c3650beb8844911303aa4a617
Coco29dev <10059@holbertonstudents.com>
2025-01-20 11:00:55 +0100

    test boucle pour task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,3 @@
 def print_list_integer(my_list=[]):
-    print("{}".format(my_list[0]))
-    print("{}".format(my_list[1]))
-    print("{}".format(my_list[2]))
-    print("{}".format(my_list[3]))
-    print("{}".format(my_list[4]))
+	for num in my_list:
+	    print("{}".format(num))
