d51f51d85fbaae6a0d7a0f6ed5578edc87c0210f
Coco29dev <10059@holbertonstudents.com>
2025-01-20 15:21:25 +0100

    rajout commentaire

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    # Parcourt chaque élément de la liste my_list et l'affiche.
+    # "{:d}".format(num) est utilisé pour formater l'élément comme un entier
     for num in my_list:
         print("{:d}".format(num))
