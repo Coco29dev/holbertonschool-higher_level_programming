521f21bb4397935d41a08742d44b95f60cfa912e
Coco29dev <10059@holbertonstudents.com>
2025-01-20 15:15:01 +0100

    rajout commentaire

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    # Si l'index est négatif, retourne une copie de la liste sans modification
     if idx < 0:
         return my_list.copy()
+    # Si l'index est supérieur ou égal à la longueur de la liste
+    # retourne une copie
     elif idx >= len(my_list):
         return my_list.copy()
+    # Si l'index est valide, crée une copie
+    # de la liste et modifie l'élément à l'index donné.
     else:
         new_list = my_list.copy()
-        new_list[idx] = element
+        new_list[idx] = element  # Remplace l'élément à l'index spécifié
         return new_list
