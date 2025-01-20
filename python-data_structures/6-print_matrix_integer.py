66487737a64a5018bbc700114605b82ed469c87a
Coco29dev <10059@holbertonstudents.com>
2025-01-20 15:10:22 +0100

    rajout commentaire

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    # Parcourt chaque ligne de la matrice (chaque "row" dans matrix)
     for row in matrix:
+        # Convertit chaque élément de la ligne en chaîne formatée et
+        # les joint avec un espace, puis les affiche.
+        # "{:d}".format(element) formate l'élément comme un entier.
+        # ' '.join() crée une chaîne avec les éléments séparés par un espace.
         print(' '.join("{:d}".format(element) for element in row))
