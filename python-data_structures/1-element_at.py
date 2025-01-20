eab0861b44e122d3ebb4012c2201bcda1b5a6e20
Coco29dev <10059@holbertonstudents.com>
2025-01-20 15:20:41 +0100

    rajout commentaire

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    # Si l'index est négatif, retourne None car un index négatif est invalide.
     if idx < 0:
         return None
+    # Si l'index est supérieur ou égal à la longueur de la liste, retourne None
     elif idx >= len(my_list):
         return None
+    # Si l'index est valide, retourne l'élément à cet index dans la liste.
     else:
         return my_list[idx]
