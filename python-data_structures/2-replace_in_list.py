0614cdacda4609b10982cfd29f1bd86a36cbf810
Coco29dev <10059@holbertonstudents.com>
2025-01-20 14:01:18 +0100

    rajout = et element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    my_list[3] = 9
-    idx = element
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return my_list
     else:
-        return my_list[idx]
+        my_list[idx] = element
+        return my_list
