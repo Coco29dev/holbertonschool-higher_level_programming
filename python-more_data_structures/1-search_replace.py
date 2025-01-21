00dae7a296762bfc2961e1d013e6e1df5cc6171b
Coco29dev <10059@holbertonstudents.com>
2025-01-21 15:04:54 +0100

    code fonctionnel avec copy. + pycodestyle task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = []
     for row in my_list:
         if row == search:
-    	    new_list.append(replace)
+            new_list.append(replace)
         else:
-    	    new_list.append(row)
+            new_list.append(row)
     return new_list
