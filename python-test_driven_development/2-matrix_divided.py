7a8ea8b53b0c4a8ea64c8324918c164b8d72a509
Coco29dev <10059@holbertonstudents.com>
2025-01-24 14:52:11 +0100

    pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -65,30 +65,29 @@ def matrix_divided(matrix, div):
         >>> matrix
         [[1, 2, 3], [4, 5, 6]]
     """
-    # Vérification que la matrice est une liste de listes d'entiers ou de flottants
     if not isinstance(matrix, list) or not all(isinstance(row, list)
-                                                for row in matrix):
+                                               for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) "
-                         "of integers/floats")
-    
+                        "of integers/floats")
+
     # Vérification que chaque ligne de la matrice a la même taille
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    
-    # Vérification que chaque élément de la matrice est un entier ou un flottant
+
+    # Vérification que chaque élément la matrice est un entier ou un flottant
     for row in matrix:
         if not all(isinstance(elem, (int, float)) for elem in row):
             raise TypeError("matrix must be a matrix (list of lists) "
-                             "of integers/floats")
-    
+                            "of integers/floats")
+
     # Vérification que le diviseur est un nombre (int ou float)
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+
     # Vérification que le diviseur n'est pas égal à zéro
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     # Diviser chaque élément de la matrice et arrondir à 2 décimales
     return [[round(elem / div, 2) for elem in row] for row in matrix]
