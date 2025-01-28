e4396ebabe0ca708ed9303e8d4e096b7d8e1cc38
Coco29dev <10059@holbertonstudents.com>
2025-01-28 10:28:54 +0100

    creation class rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Ce module définit une classe Rectangle.
+
+La classe Rectangle permet de créer des
+objets représentant des rectangles,
+avec des attributs pour la largeur et la hauteur.
+Des méthodes peuvent être ajoutées
+plus tard pour calculer des propriétés telles que la surface ou le périmètre.
+"""
+
+
+class Rectangle:
+    """
+    Représente un rectangle.
+
+    Cette classe permet de définir un rectangle par sa largeur et sa hauteur.
+    Elle offre des fonctionnalités pour manipuler ces propriétés
+    et peut être étendue pour ajouter d'autres
+    fonctionnalités liées aux rectangles.
+
+    Attributs :
+        largeur (float) : La largeur du rectangle.
+        hauteur (float) : La hauteur du rectangle.
+
+    Méthodes :
+        __init__(self, largeur, hauteur): Initialise un rectangle
+        avec la largeur et la hauteur spécifiées.
+    """
+    pass
