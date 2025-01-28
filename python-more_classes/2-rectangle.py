c9fe022aa3c601ea03229bb77fd7d82ffbd22695
Coco29dev <10059@holbertonstudents.com>
2025-01-28 17:02:40 +0100

    rajout  private

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -46,8 +46,8 @@ class Rectangle:
             height (int) : La hauteur du rectangle. Doit être un entier
             positif ou égal à zéro.
         """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
