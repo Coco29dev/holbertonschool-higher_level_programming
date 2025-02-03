77d5bc23d0c2c132d86a5faac508ee91a7a9aa8e
Coco29dev <10059@holbertonstudents.com>
2025-02-03 17:28:42 +0100

    raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module contenant la classe de base pour les opérations géométriques.
+"""
+
+
+class BaseGeometry:
+    """
+    Classe de base pour les objets géométriques.
+    """
+
+    def area(self):
+        """
+       Méthode non implémentée pour calculer l'aire.
+        """
+        raise Exception("area() is not implemented")
