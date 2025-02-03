3154a9a7f84918a126ae7984d7e312361b135e19
Coco29dev <10059@holbertonstudents.com>
2025-02-03 17:19:57 +0100

    création classe

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module contenant la classe de base pour les opérations géométriques.
+"""
+
+
+class BaseGeometry:
+    """
+    Classe de base pour les objets géométriques.
+    """
+    pass
