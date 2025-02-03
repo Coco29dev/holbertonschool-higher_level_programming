052ea6787b4c329b5f153fb817ba7ce2ee502232
Coco29dev <10059@holbertonstudents.com>
2025-02-03 17:45:55 +0100

    Type Error et value Error

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module contenant la classe de base pour les opérations géométriques.
+"""
+
+
+class BaseGeometry:
+    """
+    Classe de base pour les objets géométriques.
+    """
+
+    def area(self):
+        """
+       Méthode non implémentée pour calculer l'aire.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+    Valide que la valeur est un entier positif.
+
+    Args:
+        name (str): Le nom de l'attribut à valider.
+        value (int): La valeur à valider.
+
+    Lève :
+        TypeError : Si la valeur n'est pas un entier.
+        ValueError : Si la valeur est inférieure ou égale à 0.
+       """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
