77e051c55d0081020ced26d89478deae4247b0c2
Coco29dev <10059@holbertonstudents.com>
2025-02-05 16:08:48 +0100

    changement de l'ordre des print

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -2,13 +2,13 @@
 
 class VerboseList(list):
     def append(self, item):
-        print(f"Added {[item]} to the list.")
         super().append(item)
+        print(f"Added {[item]} to the list.")
 
     def extend(self, x):
         new_x = len(x)
-        print(f"Extended the list with {[new_x]} item.")
         super().extend(x)
+        print(f"Extended the list with {[new_x]} item.")
 
     def remove(self, item):
         print(f"Added {[item]} to the list.")
