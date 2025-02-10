3e6d7473083e445210222af25f6e36deb9c4cd7b
Coco29dev <10059@holbertonstudents.com>
2025-02-10 10:55:09 +0100

    bonne sortie read

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module fournissant une fonction pour
+lire et afficher le contenu d'un fichier texte.
+"""
+
+
+def read_file(filename=""):
+    """
+    Lit un fichier texte encodé en UTF-8 et
+    affiche son contenu sur la sortie standard.
+
+    Args:
+        filename (str): Le nom du fichier à lire. Par défaut, une chaîne vide.
+
+    Utilisation :
+        - Utilise l'instruction 'with' pour gérer les opérations sur le fichier
+        - Supposé que le fichier existe et est lisible.
+    """
+    with open("my_file_0.txt", "r") as f:
+        contenu = f.read()
+    print(contenu)
