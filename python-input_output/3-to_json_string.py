7c88c38c7265bfd2e3f6c6f2bb64b378ef7b32d8
Coco29dev <10059@holbertonstudents.com>
2025-02-10 14:06:00 +0100

    code fonctionnel convert JSON to python

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module permettant de convertir un objet en une chaîne JSON.
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Convertit un objet Python en une chaîne de caractères au format JSON.
+
+    Args:
+        my_obj (any): L'objet à convertir.
+
+    Returns:
+        str: La représentation JSON de l'objet.
+    """
+    json_string = json.dumps(my_obj)
+    print(json_string)
