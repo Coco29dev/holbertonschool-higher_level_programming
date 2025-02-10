934af3d885718a5d6ebc846f70a28b9b92b749ad
Coco29dev <10059@holbertonstudents.com>
2025-02-10 13:29:50 +0100

    code fonction write

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module permettant d'écrire du texte dans un fichier.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Écrit du texte dans un fichier en UTF-8
+    et retourne le nombre de caractères écrits.
+
+    Args:
+        filename (str): Le nom du fichier.
+        text (str): Le texte à écrire.
+
+    Returns:
+        int: Le nombre de caractères écrits.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(filename)
+        return len(text)
