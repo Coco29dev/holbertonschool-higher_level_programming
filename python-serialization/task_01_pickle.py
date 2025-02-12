65bf7f79cf3a4907eb1f9e9bb93462c5d03cb00a
Coco29dev <10059@holbertonstudents.com>
2025-02-12 14:35:54 +0100

    utilisation de getter/setter pour type variable

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -15,7 +15,7 @@ class CustomObject:
     avec des attributs de nom, âge et statut d'étudiant.
     """
 
-    def __init__(self, name: str, age: int, is_student: bool):
+    def __init__(self, name, age, is_student):
         """Initialise une nouvelle instance de CustomObject.
 
         Args:
@@ -26,6 +26,40 @@ class CustomObject:
         self.name = name
         self.age = age
         self.is_student = is_student
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Name doit être un str")
+        if name is None or name.strip() == "":
+            raise ValueError("Name doit être plein ou None")
+        self._name = name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        if not isinstance(age, int):
+            raise TypeError("Age doit être un int")
+        if age < 0:
+            raise ValueError("Age doit être >= 0")
+        self._age = age
+
+    @property
+    def is_student(self):
+        return self._is_student
+
+    @is_student.setter
+    def is_student(self, is_student):
+        if not isinstance(is_student, bool):
+            raise TypeError("is_student doit être un boolean")
+        self._is_student = is_student
 
     def display(self):
         """Affiche les attributs de l'objet."""
@@ -46,8 +80,8 @@ class CustomObject:
         try:
             with open(filename, "wb") as f:
                 pickle.dump(self, f)
-        except Exception as e:
-            print(f"Il y a une erreur lors de la sérialisation: {e}")
+        except Exception:
+            print(f"Il y a une erreur lors de la sérialisation")
             return None
 
     @classmethod
@@ -64,6 +98,6 @@ class CustomObject:
         try:
             with open(filename, "rb") as f:
                 return pickle.load(f)
-        except (FileNotFoundError, pickle.UnpicklingError) as e:
-            print(f"Il y a une erreur lors de la désérialisation: {e}")
+        except (FileNotFoundError, pickle.PickleError):
+            print(f"Il y a une erreur lors de la désérialisation")
             return None
