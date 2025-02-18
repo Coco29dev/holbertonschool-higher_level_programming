7373a867a2cebb9672c43a3f57dbd82fd23f5d97
Coco29dev <10059@holbertonstudents.com>
2025-02-18 17:48:57 +0100

    /, data, status, error

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            status = {"status": "OK"}
+            self.wfile.write(json.dumps(status).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            error_message = {"error": "Endpoint not found"}
+            self.wfile.write(json.dumps(error_message).encode('utf-8'))
+
+
+server_address = ('', 8000)
+httpd = HTTPServer(server_address, SimpleAPIHandler)
+print("Serveur démarré sur http://localhost:8000")
+httpd.serve_forever()
