aa87b5d394efcb8c058ae6a8e59ad0b48df7c126
Coco29dev <10059@holbertonstudents.com>
2025-02-19 09:05:53 +0100

    modif

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,10 +6,6 @@ import json
 
 class SimpleAPIHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/favicon.ico':
-            self.send_response(204)
-            self.end_headers()
-            return
         if self.path == "/":
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
@@ -37,16 +33,15 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(response).encode('utf-8'))
         else:
             self.send_response(404)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
-            error_message = {b"Endpoint not found"}
-            self.wfile.write(json.dumps(error_message).encode('utf-8'))
+            self.wfile.write(b"Endpoint not found")
 
 
-def run():
-    server_address = ('', 8000)
-    httpd = HTTPServer(server_address, SimpleAPIHandler)
-    print("Serveur démarré sur http://localhost:8000")
+def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Début d'API sur le port {port}")
     httpd.serve_forever()
 
 
