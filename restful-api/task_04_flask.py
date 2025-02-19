ca384ca1932ed85b378cda51412e41314bacf4dc
Coco29dev <10059@holbertonstudents.com>
2025-02-19 13:36:50 +0100

    pycodestyle

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -4,8 +4,10 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-         "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}}
+users = {"jane": {"username": "jane",
+                  "name": "Jane", "age": 28, "city": "Los Angeles"},
+         "john": {"username": "john",
+                  "name": "John", "age": 30, "city": "New York"}}
 
 
 @app.route('/')
@@ -37,9 +39,11 @@ def add_usr():
     if 'username' not in new_user:
         return jsonify({"error": "Nom d'utilisateur requis"}), 404
     username = new_user['username']
-    users[username] = {"username": new_user.get('username'), "name": new_user.get(
+    users[username] = {"username": new_user.get('username'),
+                       "name": new_user.get(
         'name'), "age": new_user.get('age'), "city": new_user.get('city')}
-    return jsonify({"message": "Utilisateur ajouté", "user": users[username]}), 201
+    return jsonify({"message":
+                    "Utilisateur ajouté", "user": users[username]}), 201
 
 
 if __name__ == "__main__":
