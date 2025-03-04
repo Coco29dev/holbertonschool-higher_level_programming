1464615ff93be66618e32839d0f22629d109dcc6
Coco29dev <10059@holbertonstudents.com>
2025-03-04 14:09:59 +0100

    update server

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create
 -- user server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON hbtn_0d_tvshows.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
