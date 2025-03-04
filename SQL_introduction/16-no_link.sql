0e20ee54798bb305937dee8cc890be11a60da1a6
Coco29dev <10059@holbertonstudents.com>
2025-03-04 09:54:08 +0100

    update

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,3 @@
 -- say
 -- my name
-INSERT INTO second_table (score, name) VALUES ('18', 'Aria');
-INSERT INTO second_table (score, name) VALUES ('12', 'Aria');
-SELECT score, name FROM second_table ORDER BY score DESC, name DESC
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
