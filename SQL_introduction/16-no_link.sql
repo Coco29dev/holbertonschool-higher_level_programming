e7448b1b3c21cbf06cb100839da315664b33bbd7
Coco29dev <10059@holbertonstudents.com>
2025-03-04 09:50:42 +0100

    say my name

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- say
+-- my name
+INSERT INTO second_table (score, name) VALUES ('18', 'Aria');
+INSERT INTO second_table (score, name) VALUES ('12', 'Aria');
+SELECT score, name FROM second_table ORDER BY score DESC, name DESC
