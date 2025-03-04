f9fdff2a1260935fd07354f923f4dd29356524d5
Coco29dev <10059@holbertonstudents.com>
2025-03-04 14:04:38 +0100

    update grant

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- create
 -- user server
-CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON hbtn_0d_tvshows.* TO 'user_0d_1'@'localhost';
