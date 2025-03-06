7e839a7ee53aad7f87e17d4e9ca83ae255aecf29
Coco29dev <10059@holbertonstudents.com>
2025-03-06 14:38:20 +0100

    list all states in py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+"""
+This script connects to a MySQL database
+and lists all states from the 'states' table,
+ordered by 'id'. It takes three arguments:
+MySQL username, password, and database name.
+"""
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+
+    mydb = MySQLdb.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="hbtn_0e_0_usa"
+    )
+
+    cursor = mydb.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for state in cursor.fetchall():
+        print(state)
+
+    cursor.close()
+    mydb.close()
