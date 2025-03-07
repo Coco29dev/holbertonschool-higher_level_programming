e85817c8c5ef7931118ec5c1c5135502e0d37c8a
Coco29dev <10059@holbertonstudents.com>
2025-03-07 15:36:01 +0100

    modif sans create engine

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,9 +5,9 @@ Defines the State class for SQLAlchemy ORM.
 """
 
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+
 
 
 Base = declarative_base()
@@ -20,9 +20,3 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-engine = create_engine("mysql+mysqldb://@localhost/")
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
