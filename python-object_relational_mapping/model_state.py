f6fc33407fc6f5471e8d6e534a60a00cb60d495d
Coco29dev <10059@holbertonstudents.com>
2025-03-07 15:28:42 +0100

    create class state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+Defines the State class for SQLAlchemy ORM.
+"""
+
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Represents a state in the database.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    engine = create_engine("mysql+mysqldb://@localhost/")
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.close()
